49f11a9bcd50fb163dbe9d970f01d070817ad6d4
lb57 <saarthaktyagi1@gmail.com>
2026-06-05 14:41:44 +0530

    tranlate

---
+++ application/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func ffd(weight []int, c int, n int) int {
+	sort.Slice(weight, func(i, j int) bool {
+		return weight[i] > weight[j]
+	})
+
+	buckets := []int{c}
+
+	for i := 0; i < n; i++ {
+		flag := false
+
+		for j := 0; j < len(buckets); j++ {
+			if buckets[j] >= weight[i] {
+				flag = true
+				buckets[j] -= weight[i]
+				break
+			}
+		}
+
+		if !flag {
+			buckets = append(buckets, c-weight[i])
+		}
+	}
+
+	return len(buckets)
+}
+
+func main() {
+	var weight []int = []int{9, 8, 2, 2, 2, 2}
+	c := 10
+	n := len(weight)
+	res := ffd(weight, c, n)
+	fmt.Println(res)
+}
